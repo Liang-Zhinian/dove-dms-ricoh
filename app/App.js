4625a12977c6a82bd5142b1d186c1f56486f7ec4
Sprite <sprite@LiangdeMacBook-Air.local>
2018-04-22 22:36:51 +0800

    add files

--- app/App.js
+++ app/App.js
@@ -6,79 +6,47 @@
 
 import React, { Component } from 'react';
 import {
-  StyleSheet,
-  AsyncStorage
+    StyleSheet,
 } from 'react-native';
 import { connect } from 'react-redux';
 
-import SplashScreen from './login/Splash';
-import LoginScreen from './login/LoginScreen';
+import LoginScreen from './security/LoginScreen';
 import AppWithNavigationState from './navigators/AppNavigator';
-import SecurityNavigator from './navigators/SecurityNavigator';
-import configureAppWithNavigationState from './navigators/AppWithNavigationState';
 
 class App extends Component<{}> {
-  constructor(props) {
-    super(props);
-    this.state = {
-      // userChecked: false,
-      isLoading: false,
-    };
-
-    AsyncStorage
-      .setItem('admin', JSON.stringify({ username: 'admin', password: 'admin' }), (error) => {
-
-        if (error == null) {
-          this.props.checkUser();
-
+    render() {
+        if (!this.props.isLoggedIn) {
+            return <LoginScreen />;
         }
-      });
 
-
-  }
-
-  render() {
-    if (!!this.props.userChecking) {
-      return <SplashScreen />;
+        return <AppWithNavigationState />;
     }
-
-    // var secNav = configureAppWithNavigationState(SecurityNavigator)
-    // return <secNav />;
-
-    // if (!this.props.isLoggedIn) {
-    //   return <secNav />;
-    // }
-
-    return <AppWithNavigationState />;
-  }
 }
 
 // 获取 state 变化
 const mapStateToProps = (state) => {
-  return {
-    userChecking: state.auth_ext.userChecking,
-    isLoggedIn: state.auth.isLoggedIn,
-  }
+    return {
+        isLoggedIn: state.auth.isLoggedIn,
+    }
 };
 
 // 发送行为
 const mapDispatchToProps = (dispatch) => {
-  return {
-    checkUser: () => { dispatch({ type: 'CHECKING_USER' }); }
-  }
+    return {
+    }
 };
 
 export default connect(
-  mapStateToProps,
-  mapDispatchToProps,
-  null,
-  {
-    withRef: true
-  }
+    mapStateToProps,
+    mapDispatchToProps,
+    null,
+    {
+        withRef: true
+    }
 )(App);
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
+    container: {
+        flex: 1,
+    },
 });
